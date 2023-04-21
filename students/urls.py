f994714cdcf4b93374b0a09d0aecf00738379231
Keenan Jajeh <kjajeh34@gmail.com>
2023-04-21 17:53:52 -0500

    hw10

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("student", include('students.urls')),
-    path('admin/', admin.site.urls)
+    path("", views.index),
 ]
